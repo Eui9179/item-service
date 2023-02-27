e7ea4b33ebb6c4fd3ab53e6d78f867748cfb49be
Eui9179 <outdark97@gmail.com>
2023-02-28 01:58:33 +0900

    thymeleaf

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -3,17 +3,18 @@ package hello.itemservice.web.basic;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Controller
 @RequestMapping("/basic/items")
-@RequiredArgsConstructor //final이 붙은 변수를 최기화 해준다.
+@RequiredArgsConstructor //final이 붙은 변수를 최기화 해준다. @Autowired
 public class BasicItemController {
     private final ItemRepository itemRepository;
 
@@ -22,6 +23,22 @@ public class BasicItemController {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "basic/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    /**
+     * 테스트용 데이터 추
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("아이폰", 10000, 10));
+        itemRepository.save(new Item("맥북", 20000, 20));
     }
 
 }
